f55447e0ad70196913d8453d195455851c87386d
Zoë Farmer <zoe@dataleek.io>
2016-03-29 12:09:02 -0600

    Framework in place. Some TODOs, but can be taken care of later. Start of infection code

--- test_infection.py
+++ test_infection.py
@@ -7,7 +7,23 @@ import networkx as nx
 import pytest
 
 class TestInfection(object):
-    @pytest.fixture
-    def network():
-        newgraph = nx.binomial_graph(10, 0.1, directed=True)
-        nx.draw(newgraph)
+    @pytest.fixture()
+    def infection(self):
+        return limitedinfection.NetworkInfection('./testnetwork.npy')
+
+    def test_init(self, infection):
+        assert(infection.networkfile == './testnetwork.npy')
+
+    def test_load(self, infection):
+        infection.load()
+        assert(infection.graph is not None)
+        assert(infection.nxgraph is not None)
+
+    def test_choice(self, infection):
+        infection.load()
+        assert(infection.choice is False)
+        infection.choose()
+        oldchoice = infection.choice
+        assert(infection.choice in infection.nxgraph.nodes())
+        infection.choose()
+        assert(oldchoice == infection.choice)
