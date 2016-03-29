e2174b9881af6d8183e68a815fd0f24783bba42b
William Farmer <willzfarmer@gmail.com>
2016-03-29 12:09:02 -0600

    Framework in place. Some TODOs, but can be taken care of later. Start of infection code

--- limitedinfection.py
+++ limitedinfection.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3.5
 
 
+"""
+Khan Academy's Limited Infection Problem
+========================================
+Some notes:
+    * Some directed edge from A->B indicates that A coaches B.
+"""
+
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,13 +16,50 @@ import networkx as nx
 
 
 def main():
-    newgraph = nx.binomial_graph(10, 0.1, directed=True)
-    nx.draw(newgraph)
+    infection = NetworkInfection()
+    infection.load()
+    infection.choose()
+    infection.show()
 
+class NetworkInfection(object):
+    def __init__(self, filename='./testnetwork.npy', refresh=False, choose_node=False):
+        self.networkfile = filename
+        self.graph       = None
+        self.nxgraph     = None
+        self.choice      = choose_node
 
-def foo():
-    return 5
+        if refresh:
+            self._gen_new_random_graph()
+            self.filename = './testnetwork.npy'
 
+    def load(self):
+        self.graph   = np.load(self.networkfile)
+        self.nxgraph = nx.DiGraph(self.graph)
+
+    def show(self):
+        plt.figure()
+        nx.draw(self.nxgraph, pos=nx.spring_layout(self.nxgraph))
+        plt.show()
+
+    def _gen_new_random_graph(self):
+        newgraph = nx.binomial_graph(20, 0.1, directed=True)
+        np.save('testnetwork.npy', nx.adjacency_matrix(newgraph).todense())
+
+    def choose(self):
+        """
+        TODO: Need to make sure choice is part of connected subgraph. (not trivial)
+        """
+        if type(self.choice) == bool:
+            if self.choice:
+                self.choice = input('Select Node')
+            else:
+                self.choice = np.random.choice(self.nxgraph.nodes())
+
+    def total_infection(self):
+        pass
+
+    def limited_infection(self):
+        pass
 
 
 
