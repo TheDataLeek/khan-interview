19f0a3b14be6a18895c07a3d3d64ced7eebd94d8
Zoë Farmer <zoe@dataleek.io>
2016-03-30 11:20:35 -0600

    Updated tests with new syntax

--- test_infection.py
+++ test_infection.py
@@ -10,7 +10,7 @@ import pytest
 class TestInfection(object):
     @pytest.fixture()
     def infection(self):
-        return limitedinfection.NetworkInfection('./testnetwork.npy')
+        return limitedinfection.NetworkInfection(50, 0.2, './testnetwork.npy')
 
     def test_init(self, infection):
         assert(infection.networkfile == './testnetwork.npy')
